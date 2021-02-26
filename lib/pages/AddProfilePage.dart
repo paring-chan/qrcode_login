1469dde21ba8a89902669d5cd1be619e50d8eb60
pikokr <parangee9706@gmail.com>
2021-02-26 12:39:27 +0900

    feat: email form field

--- lib/pages/AddProfilePage.dart
+++ lib/pages/AddProfilePage.dart
@@ -2,11 +2,35 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddProfilePage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('프로필 추가'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return '텍스트를 입력해주세요';
+                  } else
+                    return null;
+                },
+                decoration: InputDecoration(
+                  hintText: '이메일'
+                ),
+              )
+            ],
+          ),
+        )
       ),
     );
   }
