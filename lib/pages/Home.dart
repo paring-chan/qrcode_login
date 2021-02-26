5565c9faab0c5881899cf3e77d2dd50dbad286e9
pikokr <parangee9706@gmail.com>
2021-02-26 15:24:15 +0900

    버림

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -9,9 +15,17 @@ class MyApp extends StatelessWidget {
       ),
       body: Center(
           child: Column(
-        children: [OutlinedButton(onPressed: () async {
-
-        }, child: Text('계정 추가하기'))],
+        children: [
+          OutlinedButton(onPressed: () async {
+            if (await Permission.camera.request().isGranted) {
+              String result = await scanner.scan();
+              var res = json.decode(result);
+              var pref = await SharedPreferences.getInstance();
+            } else {
+              ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(content: Text('카메라 사용 권한을 허용해주세요.')));
+            }
+          }, child: Text('계정 추가하기'))
+        ],
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
       )),
