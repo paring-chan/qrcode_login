d23f4c00c3dae293dbfc0a42bce7ca5deed8ec9b
pikokr <parangee9706@gmail.com>
2021-02-26 15:07:59 +0900

    feat: install qrscan

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    return
-      Scaffold(
-        appBar: AppBar(
-          title: Text('QRCODE LOGIN'),
-        ),
-        body: Center(
-            child: Text('test')
-        ),
-      );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QRCODE LOGIN'),
+      ),
+      body: Center(
+          child: Column(
+        children: [OutlinedButton(onPressed: () async {
+
+        }, child: Text('계정 추가하기'))],
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+      )),
+    );
   }
 }
