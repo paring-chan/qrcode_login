84069fa9fcd14b447a95820ad306c281da92b38f
pikokr <parangee9706@gmail.com>
2021-02-26 20:21:00 +0900

    feat: dialog

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -6,9 +6,13 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:qrlogin/db.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
-import 'package:sqflite/sqflite.dart';
 
 class MyApp extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
@@ -18,56 +22,86 @@ class MyApp extends StatelessWidget {
       body: Center(
           child: Column(
         children: [
-          OutlinedButton(onPressed: () async {
-            if (await Permission.camera.request().isGranted) {
-              try {
-                String result = await scanner.scan();
-                var res = json.decode(result);
-                var db = await DB.getDB();
-                var data = await showDialog(context: ctx, builder: (context) {
-                  return AlertDialog(
-                    title: Text('계정 등록'),
-                    content: Form(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: '이메일'
+          OutlinedButton(
+              onPressed: () async {
+                if (await Permission.camera.request().isGranted) {
+                  try {
+                    String result = await scanner.scan();
+                    var res = json.decode(result);
+                    if (res['RequestURL'] == null) throw Error();
+                    var db = await DB.getDB();
+                    var data = await showDialog(
+                        context: ctx,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('계정 등록'),
+                            content: Form(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextFormField(
+                                    decoration:
+                                        InputDecoration(hintText: '이메일'),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return '이메일을 입력해주세요.';
+                                      }
+                                      return null;
+                                    },
+                                    controller: _emailController,
+                                  ),
+                                  TextFormField(
+                                    decoration:
+                                        InputDecoration(hintText: '비밀번호'),
+                                    obscureText: true,
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return '비밀번호를 입력해주세요.';
+                                      }
+                                      return null;
+                                    },
+                                    controller: _passwordController,
+                                  ),
+                                ],
+                              ),
+                              key: _formKey,
                             ),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                                hintText: '비밀번호'
-                            ),
-                            obscureText: true,
-                          ),
-                        ],
-                      ),
-                    ),
-                    actions: [
-                      TextButton(onPressed: () {
-                        Navigator.pop(context, "Cancel");
-                      }, child: Text('취소')),
-                      TextButton(onPressed: () {
-                        Navigator.pop(context);
-                      }, child: Text('저장'))
-                    ],
-                  );
-                },barrierDismissible: false);
-                if (data == 'Cancel') {
-                  Fluttertoast.showToast(msg: '취소되었습니다.');
-                  return;
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context, "Cancel");
+                                  },
+                                  child: Text('취소')),
+                              TextButton(
+                                  onPressed: () {
+                                    if (_formKey.currentState.validate()) {
+                                      Navigator.of(context).pop();
+                                    }
+                                  },
+                                  child: Text('저장'))
+                            ],
+                          );
+                        },
+                        barrierDismissible: false);
+                    if (data == 'Cancel') {
+                      Fluttertoast.showToast(msg: '취소되었습니다.');
+                      await db.close();
+                      return;
+                    }
+                    log(res['RequestURL']);
+                    log(_emailController.text);
+                    log(_passwordController.text);
+                    await db.close();
+                  } catch (error) {
+                    log(error.toString());
+                    Fluttertoast.showToast(msg: "QR코드 파싱중 오류가 발생했습니다.");
+                  }
+                } else {
+                  ScaffoldMessenger.of(ctx).showSnackBar(
+                      SnackBar(content: Text('카메라 사용 권한을 허용해주세요.')));
                 }
-                // await db.close();
-              } catch (error) {
-                log(error.toString());
-                Fluttertoast.showToast(msg: "QR코드 파싱중 오류가 발생했습니다.");
-              }
-            } else {
-              ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(content: Text('카메라 사용 권한을 허용해주세요.')));
-            }
-          }, child: Text('계정 추가하기'))
+              },
+              child: Text('계정 추가하기'))
         ],
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
