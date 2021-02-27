eb238b3dffd47842b082a1a64a773c750670306d
pikokr <parangee9706@gmail.com>
2021-02-27 11:09:19 +0900

    feat: ProfileTile class

---
+++ lib/structures/ProfileTile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileTile extends StatelessWidget {
+  final Map<String, dynamic> profile;
+
+  ProfileTile(this.profile);
+
+  @override
+  Widget build(BuildContext context) {
+  }
+}
