800aed2ccece95aacfc575d5e2f1eaa003b57e67
pikokr <parangee9706@gmail.com>
2021-02-27 09:42:05 +0900

    feat: make DeleteProfiles stateful

--- lib/pages/DeleteProfiles.dart
+++ lib/pages/DeleteProfiles.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DeleteProfiles extends StatelessWidget {
+class DeleteProfiles extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DeleteProfiles();
+}
+
+class _DeleteProfiles extends State<DeleteProfiles> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
