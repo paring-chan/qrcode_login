20e4e1ecb39eddbb92d3ba4ab6b1cdaacc3ebd7e
pikokr <parangee9706@gmail.com>
2021-02-27 10:24:48 +0900

    feat: get profile list

--- lib/pages/DeleteProfiles.dart
+++ lib/pages/DeleteProfiles.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qrlogin/db.dart';
 
 class DeleteProfiles extends StatefulWidget {
   @override
@@ -7,12 +10,32 @@ class DeleteProfiles extends StatefulWidget {
 }
 
 class _DeleteProfiles extends State<DeleteProfiles> {
+  var profiles = <Map<String, dynamic>>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('프로필 삭제하기'),
       ),
+      body: Column(
+        children: [],
+      ),
     );
+  }
+
+  @override
+  initState() {
+    super.initState();
+    load();
+  }
+
+  Future load() async {
+    var db = await DB.getDB();
+    try {
+      var profiles = await db.query('profiles');
+    } finally {
+      await db.close();
+    }
   }
 }
